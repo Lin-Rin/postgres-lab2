da7aa29c41b32726408f8b6a1a2272b853a08929
LTX <qyvjc1@gmail.com>
2023-10-21 14:18:59 +0300

    delete redundant lines

--- src/main/resources/init-db.sql
+++ src/main/resources/init-db.sql
@@ -4,15 +4,4 @@ CREATE TABLE user_counter (
     version BIGINT
 );
 
-INSERT INTO user_counter (user_id, counter, version) VALUES (?, ?, ?);
-
 SELECT * FROM user_counter;
-
-UPDATE user_counter SET counter=1, version=1 WHERE user_id=4 AND version=0;
-
--- drop table user_counter;
-
--- CREATE TABLE table1 (number BIGINT);
--- INSERT INTO table1 (number) VALUES (1);
--- INSERT INTO table1 (number) VALUES (2);
--- SELECT * FROM table1;
